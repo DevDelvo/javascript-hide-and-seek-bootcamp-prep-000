a5443ee2cbd6782b089bd79c8ed4255dbf891ebc
Kevin Delvo <kevinbdelvo@gmail.com>
2018-02-06 21:20:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,8 @@ function deepestChild() {
 
 function increaseRankBy(n) {
   const lis = document.getElementById('ranked-list').querySelectorAll('ul.ranked-list li');
+  
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
 }
