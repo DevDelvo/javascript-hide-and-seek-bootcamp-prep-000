8e65848026c4184206721c8eecf20beb60abcae9
Kevin Delvo <kevinbdelvo@gmail.com>
2018-02-06 21:44:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+  const nodes = document.querySelectorAll('#grand-node div');
+  var deepest = nodes[nodes.length-1];
+  return deepest;
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
